3dcdbf391183eadebbc3d66d5844275e6714fb5f
Dani <daniel@latostadora.com>
2018-09-14 11:29:25 +0200

    Refactor code structure split by item types

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -9,51 +9,60 @@ this.items = [];
 function update_quality() {
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
-        var isSulfuras = item.name == 'Sulfuras, Hand of Ragnaros';
+        var isSulfuras = item.name === 'Sulfuras, Hand of Ragnaros';
+        var isBackstage = item.name === 'Backstage passes to a TAFKAL80ETC concert';
+        var isAgedBrie = item.name === 'Aged Brie';
+
         if(isSulfuras) {
             continue;
         }
 
-        var isBackstage = item.name == 'Backstage passes to a TAFKAL80ETC concert';
-        var isAgedBrie = item.name == 'Aged Brie';
-
-        if (isAgedBrie || isBackstage) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-                if (isBackstage) {
-                    if (item.sell_in <= 10) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                    if (item.sell_in <= 5) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                }
-            }
-        } else {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1
-            }
-        }
-
         item.sell_in = item.sell_in - 1;
 
-        if (item.sell_in < 0) {
-            if (!isAgedBrie) {
-                if (!isBackstage) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1
-                    }
-                } else {
-                    item.quality = 0
-                }
-            } else {
+        if (isAgedBrie) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+
+            if (item.sell_in < 0) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality = item.quality + 1;
                 }
+            }
+
+            continue;
+        }
+
+        if (isBackstage) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+                if (item.sell_in < 10) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                if (item.sell_in < 5) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+
+            if (item.sell_in < 0) {
+                item.quality = 0;
+            }
+
+            continue;
+        }
+
+
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+
+        if (item.sell_in < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
